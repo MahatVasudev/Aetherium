d41f4bfec66f1f2dd3ee091013666484ace54c2f
MahatVasudev <mahatvasudev9@gmail.com>
2026-01-25 18:47:54 +0530

    Basic Commits

--- core-component/core/src/main.rs
+++ core-component/core/src/main.rs
@@ -1,3 +1,7 @@
+// TODO: Make a File system Manager
+// It should Make a toml file named codex.toml
+// It should make some other folders like data/
+
 fn main() {
     println!("Hello, world!");
 }
