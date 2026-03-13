9189716620d95d2b447e708942f25f905bb21bca
MahatVasudev <mahatvasudev9@gmail.com>
2026-03-13 08:50:08 +0530

    Added New Dependancies + Made CLI Partially Operational

--- core-component/cli/src/main.rs
+++ core-component/cli/src/main.rs
@@ -5,31 +5,25 @@
 
 // TODO: AFTER ENGINE IMPLEMENTATION: Interaction with the engine, and print when the work is don
 
+use aetherium_engine::{Engine, EngineRequest};
 use clap::Parser;
-use cli::{CLI, Commands, commands::CodexCmd};
+use cli::{
+    CLI, Commands,
+    commands::{CodexCmd, Runnable},
+};
 // TODO: MAKE IT PRETTY
-fn main() {
-    let cli = CLI::parse();
 
-    match cli.command {
-        Commands::Create(cmd) => {
-            println!("Create: {}", cmd.codex)
-        }
-        Commands::Ask(val) => {
-            println!(
-                "Ask: {}, options ignore_case: {}, mode: {:?}, asked results: {}",
-                val.query, val.ignore_case, val.mode, val.top_k
-            )
-        }
-        Commands::Search(cmd) => {
-            println!("Search Query: {}, files: {:?}", cmd.query, cmd.files)
-        }
-        Commands::Codex(cmd) => match cmd {
-            CodexCmd::Add(val) => println!("Adding File: {}", val.file),
-            CodexCmd::Delete(val) => println!(
-                "Deleting File: {}, with conformation: {}",
-                val.file, val.conformation
-            ),
-        },
+#[tokio::main(flavor = "multi_thread")]
+async fn main() {
+    let cli = CLI::parse();
+    let result = match cli.command {
+        Commands::Create(cmd) => cmd.run().await,
+        Commands::Codex(cmd) => cmd.run().await,
+        Commands::Config(cmd) => cmd.run().await,
+    };
+
+    if let Err(e) = result {
+        eprintln!("Error received: {}", e);
+        std::process::exit(1);
     }
 }
