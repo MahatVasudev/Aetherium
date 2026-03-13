5e11b52b6744da2901c8b250bbf12adc2107fdef
MahatVasudev <mahatvasudev9@gmail.com>
2026-03-13 08:41:36 +0530

    Added a basic config for ml server

---
+++ core-component/core/src/ml_server/config.rs
@@ -0,0 +1,44 @@
+use serde::{Deserialize, Serialize};
+
+use crate::{CONFIG_DIR, ConfigError};
+
+#[derive(Deserialize, Serialize)]
+#[serde(default)]
+pub struct MLConfig {
+    pub host: String,
+    pub port: u16,
+    pub model: String,
+    pub version: u32,
+}
+
+impl Default for MLConfig {
+    fn default() -> Self {
+        Self {
+            host: "localhost".to_string(),
+            port: 50032,
+            version: 1,
+            model: "all-MiniLM-L6-v2".to_string(),
+        }
+    }
+}
+
+impl MLConfig {
+    pub fn load() -> Result<Self, ConfigError> {
+        let path = CONFIG_DIR
+            .as_ref()
+            .ok_or(ConfigError::NoDirsFound)?
+            .join("ml.toml");
+
+        if !path.exists() {
+            return Ok(MLConfig::default());
+        }
+
+        let content = std::fs::read_to_string(path)?;
+
+        Ok(toml::from_str(&content)?)
+    }
+
+    pub fn address(&self) -> String {
+        format!("http://{}:{}", self.host, self.port)
+    }
+}
