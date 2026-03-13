5ff18ffdc732766d63091bd7b00f3cfeefc5bf36
MahatVasudev <mahatvasudev9@gmail.com>
2026-03-13 08:48:40 +0530

    Error Types for Engine

---
+++ core-component/engine/src/error/mod.rs
@@ -0,0 +1,55 @@
+use std::fmt::Display;
+
+use crate::EngineResponse;
+
+#[derive(Debug)]
+pub enum EngineError {
+    MLServerUnavailable(String),
+    Corrupt(String),
+    ConnectionError(tonic::transport::Error),
+    GRPCError(tonic::Status),
+}
+
+impl EngineError {
+    pub fn message(&self) -> String {
+        match self {
+            Self::MLServerUnavailable(v) => format!("ML Server Unavailable: {v}"),
+            Self::Corrupt(v) => format!("Something has been corrupted: {v}"),
+            Self::ConnectionError(v) => format!("GRPC connection error: {}", v.to_string()),
+            Self::GRPCError(v) => format!("GRPC status error: {}", v.to_string()),
+        }
+    }
+}
+
+impl Display for EngineError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            _ => write!(f, "{}", self.message()),
+        }
+    }
+}
+
+impl std::error::Error for EngineError {}
+
+impl From<EngineError> for EngineResponse {
+    fn from(value: EngineError) -> Self {
+        match value {
+            EngineError::MLServerUnavailable(v) => Self::MLUnavailable(v),
+            _ => Self::Error {
+                message: value.message(),
+            },
+        }
+    }
+}
+
+impl From<tonic::transport::Error> for EngineError {
+    fn from(value: tonic::transport::Error) -> Self {
+        Self::ConnectionError(value)
+    }
+}
+
+impl From<tonic::Status> for EngineError {
+    fn from(value: tonic::Status) -> Self {
+        Self::GRPCError(value)
+    }
+}
