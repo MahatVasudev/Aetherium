3cfe88455cc8b43ad429a50bb0cd49d2277faf5c
MahatVasudev <mahatvasudev9@gmail.com>
2026-04-10 17:46:14 +0530

    useless commit

--- core-component/core/bulletin-board.rs
+++ core-component/core/bulletin-board.rs
@@ -2,38 +2,34 @@
 // DATABASE CORE
 // ==========================================
 
-// TODO: Add an Option for user to add the data as mutable or immutable
-// TODO: Design schema (files, metadata, cache)
-// TODO: Add schema version table
-// TODO: Add migration system
-// TODO: Add transaction helpers
-
 // ==========================================
 // STORAGE CONTRACT
 // ==========================================
-
-// TODO: Define source of truth (filesystem authoritative)
-// TODO: Define file metadata model (hash, size, mtime)
+//
+// TODO: Add functionality to add multiple files from cli
 // TODO: Define fatal vs recoverable errors
 
 // ==========================================
 // SQLITE METHODS
 // ==========================================
 
-// TODO: Add file insert/update/delete methods
 // TODO: Add cache insert/get/invalidate methods
 
 // ==========================================
 // SYNCHRONIZATION
 // ==========================================
 
-// TODO: Implement Storage::sync()
 // TODO: Add rebuild-from-filesystem method
-// TODO: Add SyncReport struct
 
 // ==========================================
 // SEARCH / RANKING
 // ==========================================
 
-// TODO: Implement streamed TF-IDF
-// TODO: Add inverted index table
+// ==========================================
+// CLUSTERING
+// ==========================================
+//
+// TODO: Add functionality of clustering (manually? or automatically?)
+// TODO: Add Clustring sqlite table to track cluster
+//
+// TODO: FUTURE: Record Cluster Drift
